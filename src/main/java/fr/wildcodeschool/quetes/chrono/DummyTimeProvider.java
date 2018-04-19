4013e282fe89113cd671356ff5b02971416008c1
Lucile Diague <lucile.diague0@orange.fr>
2018-04-19 12:13:13 +0200

    update chrono

--- src/main/java/fr/wildcodeschool/quetes/chrono/DummyTimeProvider.java
+++ src/main/java/fr/wildcodeschool/quetes/chrono/DummyTimeProvider.java
@@ -1,38 +1,79 @@
 package fr.wildcodeschool.quetes.chrono;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class DummyTimeProvider implements TimeProvider {
+    private Long totalTime;
+    private LocalDateTime startTime = LocalDateTime.now();
+    private Boolean start = false;
+    private boolean reset = false;
+    private Long delay = 0L;
+
     @Override
     public void startStop() {
-        // nothing
+        if (!start) {
+            start = true;
+            if (reset) {
+                startTime = LocalDateTime.now();
+                reset = false;
+            }
+        } else {
+            start = false;
+        }
     }
 
     @Override
     public void reset() {
-        // nothing
+        this.delay = 0L;
+        this.totalTime = 0L;
+        if (start) {
+            this.startTime = LocalDateTime.now();
+            this.reset = false;
+        }
+        else {
+            this.reset = true;
+        }
+    }
+
+    public DummyTimeProvider(Long elapsed) {
+        this.delay = elapsed;
+        this.totalTime = elapsed;
     }
 
     @Override
     public boolean isStarted() {
-        return true;
+        if (start) {
+            Duration gap = Duration.between(startTime,LocalDateTime.now().plusSeconds(delay));
+            this.totalTime = gap.toSeconds();
+            return true;
+        }
+        else {
+            if (reset) {
+                return true;
+            } else {
+                startTime = startTime.plusSeconds(1);
+                return false;
+            }
+        }
     }
-
     @Override
     public long getSecondsTotalRuntime() {
-        return 370;
+        return this.totalTime;
     }
 
     @Override
     public long getHoursRuntime() {
-        return 0;
+        return totalTime / 3600;
     }
 
     @Override
     public long getMinutesRuntime() {
-        return 6;
+        return (totalTime / 60) % 60;
     }
 
     @Override
     public long getSecondsRuntime() {
-        return 10;
+        return totalTime % 60;
     }
 }
